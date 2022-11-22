afba68a53293d27dbc6c505a13cd8cdb5b684627
daniil <podryadov89@mail.ru>
2022-11-22 22:40:49 +0500

    lr1

--- src/Timus/Task1409.java
+++ src/Timus/Task1409.java
@@ -1,22 +1,20 @@
 package Timus;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Task1409 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int g = in.nextInt();
+        int l = in.nextInt();
+        int itg = g+l-1;
+        int itgg=itg-g;
+        int itgl=itg-l;
 
-        System.out.println("Сколько банок подстрелил Гарри?");
-        int bank1 = in.nextInt();
-
-        System.out.println("Сколько банок подстрелил Ларри?");
-        int bank2 = in.nextInt();
-
-        int it = bank2+bank1-1;
-        int itgg = it-bank1;
-        int itgl = it-bank2;
-
-        System.out.println(itgg + " " + itgl);
+        out.println(itgg+" "+itgl);
+        out.flush();
 
     }
 }
